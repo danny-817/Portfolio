0fe3d1132c018c619fb1cb84b462165192d5cf54
danny-817 <d_halsall@hotmail.com>
2023-12-05 11:18:10 +0000

    added some text to about section, centered surname container

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,16 +1,18 @@
 export default function About() {
   return (
     <div className="landing-page scroll-snap">
-      <div className="firstname-container">
+      <div></div>
+      <div className="firstname-container highlight">
         <h1 className="firstname neon-glow-pink ">Daniel</h1>
       </div>
-      <div className="surname-container">
+      <div className="surname-container highlight">
         <h1 className="surname neon-glow-blue">Halsall</h1>
       </div>
-      <p className="title neon-glow-pink ">
-        Hi, I'm Dan, thanks for taking a look at my first portfolio! <br></br>
+      <p className="title neon-glow-pink  ">
+        Hi, I'm Dan, thanks for taking a look at my portfolio <br></br>
         <br></br>I'm a junior full stack developer, having recently completed an
-        intensive full-time 13 week coding bootcamp
+        intensive full-time 13 week coding bootcamp, now looking to start a
+        productive career in tech
       </p>
     </div>
   );
