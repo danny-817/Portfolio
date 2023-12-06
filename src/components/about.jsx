642be18f4a7704562f2d56c250ebe68287d4afaa
danny-817 <d_halsall@hotmail.com>
2023-12-06 13:14:23 +0000

    fixed names spacing issue

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,13 +1,13 @@
 export default function About() {
   return (
     <div className="landing-page scroll-snap">
-      <div className="firstname-container highlight  ">
+      <div className="firstname-container  highlight ">
         <h1 className="firstname neon-glow-blue ">Daniel</h1>
       </div>
       <div className="surname-container highlight ">
         <h1 className="surname neon-glow-blue  ">Halsall</h1>
       </div>
-      <p className="title neon-glow-blue  ">
+      <p className=" neon-glow-blue  ">
         Hi, I'm Dan, thanks for taking a look at my portfolio <br></br>
         <br></br>I'm a junior full stack developer, having recently completed an
         intensive full-time 13 week coding bootcamp, now looking to start a
