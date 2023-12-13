d7ced813959ea10a8121a6b0b6e95168716b62d5
danny-817 <d_halsall@hotmail.com>
2023-12-13 23:44:51 +0000

    thumbnails animate when they come into and go out of view

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,53 +1,29 @@
-import React, { useEffect, useRef, useState } from "react";
-
 import { Link } from "react-router-dom";
+import { useRef, useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 export default function Projects() {
-  const [visibleThumbnails, setVisibleThumbnails] = useState([]);
+  const options = { rootMargin: "0px 0px -200px 0px " };
+  const [ref1, inView1] = useInView(options);
+  const [ref2, inView2] = useInView(options);
+  const [ref3, inView3] = useInView(options);
 
-  const handleIntersection = (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        setVisibleThumbnails((prevVisible) => [...prevVisible, entry.target]);
-      }
-    });
-  };
+  console.log(inView1, inView2, inView3);
 
-  const observer = new IntersectionObserver(handleIntersection, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.5, // Adjust this threshold as needed
-  });
-
-  const thumbnailRefs = useRef([]);
-
-  useEffect(() => {
-    thumbnailRefs.current.forEach((thumbnailRef) => {
-      observer.observe(thumbnailRef);
-    });
-
-    return () => {
-      thumbnailRefs.current.forEach((thumbnailRef) => {
-        observer.unobserve(thumbnailRef);
-      });
-    };
-  }, [thumbnailRefs, observer]);
   return (
     <div className=" ">
       <h1 className="title neon-glow-blue">Projects</h1>
       <div className="projects-container ">
-        <div className="project">
+        <div className="project animation-container">
           <Link to="https://www.cssscript.com/demo/cyberpunk-2077/#section-banners">
-            <h2 className="project-title neon-glow-blue">SnackTrack</h2>
+            <h2 className="project-title neon-glow-blue" ref={ref1}>
+              SnackTrack
+            </h2>
 
             <img
-              className={`thumbnail box-shadow  ${
-                visibleThumbnails.includes(thumbnailRefs.current[0])
-                  ? "visible"
-                  : ""
-              }`}
-              src="src/assets/snacktrack.png
-"
+              className={`thumbnail box-shadow
+              ${inView1 ? `animated-image-on` : `animated-image-off`} `}
+              src="src/assets/snacktrack.png"
               alt="placeholder"
             />
           </Link>
@@ -61,9 +37,12 @@ export default function Projects() {
         </div>
         <div className="project">
           <Link to="https://www.cssscript.com/demo/cyberpunk-2077/#section-banners">
-            <h2 className="project-title neon-glow-blue ">NC News</h2>
+            <h2 ref={ref2} className="project-title neon-glow-blue ">
+              NC News
+            </h2>
             <img
-              className="thumbnail box-shadow animation "
+              className={`thumbnail box-shadow
+            ${inView2 ? `animated-image-on` : `animated-image-off`} `}
               src="http://via.placeholder.com/640x360
 "
               alt="placeholder"
@@ -77,9 +56,12 @@ export default function Projects() {
         </div>
         <div className="project">
           <Link to="https://www.cssscript.com/demo/cyberpunk-2077/#section-banners">
-            <h2 className="project-title neon-glow-blue">NC News API</h2>
+            <h2 ref={ref3} className="project-title neon-glow-blue">
+              NC News API
+            </h2>
             <img
-              className="thumbnail box-shadow animation"
+              className={`thumbnail box-shadow
+            ${inView3 ? `animated-image-on` : `animated-image-off`} `}
               src="http://via.placeholder.com/640x360
 "
               alt="placeholder"
