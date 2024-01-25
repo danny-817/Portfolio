62b7df35159dd0b906ed027e71a63f386335f959
Daniel Halsall <dhalsall@Daniels-MacBook-Pro.local>
2024-01-25 21:45:31 +0000

    code cleaned up, comments deleted, unused fileas deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 
-import "./App.css";
-import Footer from "./components/footer";
 import About from "./components/about";
 import Projects from "./components/projects";
 import Navbar from "./components/navbar";
-// import Contact from "./components/contact";
+import Contact from "./components/contact";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,22 +14,16 @@ function App() {
         <main className="">
           <div className="" id="navbar">
             <Navbar />
-          </div>
-          {/* <div className="" id="footer">
-            <Footer />
-          </div> */}
+          </div>         
           <div className="" id="about" dir="ltr">
             <About />
           </div>
-          {/* <div className="fade-to-black full-page"></div> */}
-
-          {/* <Navbar id="navbar" className="w-50" /> */}
           <div className="projects-page lines " id="projects">
             <Projects />
           </div>
-          {/* <div id="contact">
+          <div id="contact">
             <Contact />
-          </div> */}
+          </div>
         </main>
       </div>
     </div>
