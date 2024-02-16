cbcd6a5aa164a342715ce66c388f34030b15836d
Daniel Halsall <dhalsall@Daniels-MacBook-Pro.local>
2024-02-16 15:57:58 +0000

    styling changed to accomodate safari

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,12 @@
 export default function Navbar() {
 	return (
 		<nav className="navbar">
-			<div className=" " tabIndex="1">
+			<div className=" ">
 				<a className="neon-glow-blue button button-text " href="#about">
 					Top
 				</a>
 			</div>
-			<div className="" tabIndex="1">
+			<div className="">
 				<a
 					className="neon-glow-blue button button-text"
 					href="#projects"
@@ -14,12 +14,12 @@ export default function Navbar() {
 					Projects
 				</a>
 			</div>
-			<div className="" tabIndex="1">
+			<div className="">
 				<a
 					className="neon-glow-blue button button-text "
 					href="#extras"
 				>
-					Extras
+					CV/Links
 				</a>
 			</div>
 		</nav>
